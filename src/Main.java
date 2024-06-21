e673b8ed6d7f26ca778e04e50d363ea5908112fc
Michele Angeletti <m.angeletti99@gmail.com>
2024-06-21 09:49:31 +0200

    esercizio

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(checkString("Questa è una stringa"));
+    }
+
+    private static String checkString(String value) {
+        String result;
+        /* Da sostituire
+        if (value.length() >= 10) {
+            result = "Lunghezza maggiore o uguale di 10";
+        } else {
+            result = "Lunghezza minore di 10";
+        }*/
+
+        // Sostituzione con l'operatore ternario
+        result = value.length() >= 10 ? "Lunghezza maggiore o uguale di 10" : "Lunghezza minore di 10";
+        return result;
+    }
+}
